aaa7d0795d91c50a11c838ff80b30032cd55fbb1
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-18 14:24:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function countDown() {
+    let number = 10;
+    while (number > -1) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown()
+
+function writeCards() {
+    let names = ["Guadalupe", "Ollie", "Aki"];
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return (messages);
+}
